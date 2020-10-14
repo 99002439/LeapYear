a7608e546c208c99c13f21d2dcd6a7b4caff75d1
99002439 <71487036+99002439@users.noreply.github.com>
2020-10-14 12:15:56 +0530

    Update leap-test.py

--- leap-test.py
+++ leap-test.py
@@ -1,1 +1,11 @@
 
+import unittest
+from leapYear import findLeap
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(findLeap(2020), True)
+        self.assertEqual(findLeap(2019), False)
+        
+if __name__ == '__main__':
+    unittest.main()
