4874120bede72810734347862703c6ff3830a469
99002439 <71487036+99002439@users.noreply.github.com>
2020-10-14 15:50:38 +0530

    Update leap-test.py

--- leap-test.py
+++ leap-test.py
@@ -4,7 +4,7 @@ from leapYear import leapYear
 
 class MyTest(unittest.TestCase):
     def test_my_function(self):
-        self.assertEqual(leapYear(2020), True)
+        self.assertEqual(leapYear(2020), False)
         self.assertEqual(leapYear(2019), False)
         
 if __name__ == '__main__':
